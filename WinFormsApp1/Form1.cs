a8cf0df6b219e0e0945c8029beba66a310f55923
Caioa <Volkinzxs@gmail.com>
2025-10-16 19:37:50 -0300

    loadwing()

--- WinFormsApp1/Form1.cs
+++ WinFormsApp1/Form1.cs
@@ -1459,6 +1459,7 @@ namespace WinFormsApp1
         private void ComboWingType_SelectedIndexChanged_1(object sender, EventArgs e)
         {
             string selectedType = ComboWingType.SelectedItem?.ToString() ?? "";
+            LoadWingImage("Cima");
             LoadWingControl(selectedType);
             if (!string.IsNullOrEmpty(selectedType))
             {
@@ -1466,8 +1467,7 @@ namespace WinFormsApp1
                 LabelWingType.Text = selectedType;
             }
             TestCurrentWingControl();
-
-
+            
         }
 
         private void ComboWingTypeSelection_SelectedIndexChanged(object sender, EventArgs e)
